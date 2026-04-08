6d62f85613cbc356d4f6113bc04fceca79633db6
Hasegos <csw020106@naver.com>
2026-04-09 01:06:13 +0900

    FIX templates 수정
    주석 추가

--- core/templates.py
+++ core/templates.py
@@ -1,3 +1,7 @@
 from fastapi.templating import Jinja2Templates
 
+# ───────────────────
+# 1. HTML 템플릿 설정
+# ───────────────────
+# Jinja2 엔진을 사용하여 'templates' 디렉토리 내의 HTML 파일들을 로드합니다.
 templates = Jinja2Templates(directory="templates")
