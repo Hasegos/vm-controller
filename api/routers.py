37068a926f33e16d2dcf10f6112e88870ebfece8
Hasegos <csw020106@naver.com>
2026-04-12 19:12:17 +0900

    FIX router 수정
    terminal router 추가

--- api/routers.py
+++ api/routers.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from .endpoints import user, dashboard
+from .endpoints import user, dashboard, terminal
 
 api_router = APIRouter()
 
 api_router.include_router(user.router, prefix="", tags=["users"])
 api_router.include_router(dashboard.router, prefix="", tags=["dashboard"])
+api_router.include_router(terminal.router, prefix="", tags=["terminal"] )
